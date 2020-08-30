5f3c6e1575553d7628dc9027aa6800c6c3de47c1
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-30 21:31:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,8 +3,8 @@ def position_taken?(board, index)
     false
   elsif board[index] != " " or ""
     true
-  elsif board[index] != "X" || board[index] != "O"
-    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
 
 end
 end
